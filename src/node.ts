eeb24578e5cf0e6942a33a3f4e52442767398a90
Deko Moon <44269025+OwenTruong@users.noreply.github.com>
2022-10-21 10:49:39 -0400

    Added typescript and ramda to project

---
+++ src/node.ts
@@ -0,0 +1,3 @@
+import * as R from "ramda";
+
+console.log(R.add(3, 5));
--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,10 +1,34 @@
 const path = require('path');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/main.js',
+// TODO: I believe I need a tsconfig to use typescript
+//      After that, I need to add ts compiler to webpack.
+const nodeConfig = {
+  name: "Node",
+  mode: process.env.NODE_ENV || 'development',
+  target: 'node',
+  entry: './src/node.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader'
+        }
+      }
+    ]
+  },
+
+  devtool: false,
+
+  resolve: {
+    extensions: ['.ts', '.js']
   }
-}
+};
+
+module.exports = nodeConfig
