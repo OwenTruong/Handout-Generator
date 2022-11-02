df87be4a6b72c0dd4aad02b136eaecd87b73807f
Deko Moon <44269025+OwenTruong@users.noreply.github.com>
2022-11-02 14:52:06 -0400

    work on textfield class

--- src/modules/classes/TextfieldC.ts
+++ src/modules/classes/TextfieldC.ts
@@ -1,8 +1,24 @@
-import { PDFDocument, PDFPage, PDFImage } from "pdf-lib";
+import { PDFDocument, PDFPage, PDFImage, PDFForm } from "pdf-lib";
 
 import { checkData } from "@/functions/checkData";
 import { checkType } from "@/functions/checkType";
 
 export class TextfieldC {
-  
+  x: number;
+  y: number;
+
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+  }
+
+  drawTextfield(pdfDoc: PDFDocument, page: PDFPage) {
+    const form: PDFForm = pdfDoc.getForm();
+    const textfield = form.createTextField('_');
+    textfield.setText('Enter Here');
+
+    // TODO: How to set width and height?
+
+    textfield.addToPage(page, { x: this.x, y: this.y });
+  }
 }
--- src/node.ts
+++ src/node.ts
@@ -8,6 +8,7 @@ async function test(dstPDF: string, imgsPath: string) {
   // Dynamically pick image folder
   // TODO: Have PDF function accept landscape
   // TODO: edit pdf.init or pdf.createPDF to enable a landscape page
+  // TODO: Have PDF accept zero image, line or textfield
   const pdf = new PDF();
   await pdf.init(Defaults.d3_print_portrait);
   // await pdf.init(Defaults.d3_print_landscape);
