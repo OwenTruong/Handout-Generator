7d635b7e9b9e3e601dcc21afca5d99eeec5b367a
Deko Moon <44269025+OwenTruong@users.noreply.github.com>
2022-11-23 11:37:06 -0500

    get one image template set up

--- src/CreatePDFModule/defaults/OneImage/portrait.ts
+++ src/CreatePDFModule/defaults/OneImage/portrait.ts
@@ -1,1 +1,29 @@
-export {};
+import { pWidth, pHeight } from '@constants/constants';
+import { createImage } from '@defaults/functions';
+import { img_width, img_height } from '@defaults/OneImage/oneImage';
+
+const lineImgSpace = 30;
+
+const img_x = (pWidth - img_width) / 2;
+const img_y1 = pHeight - 50 - img_height;
+const img_y2 = img_y1 - (img_height + lineImgSpace) - img_height / 2 - 10;
+
+// TODO: Add empty, field and lines to oneimage
+
+export const d1_print_portrait = {
+  id: 10,
+  name: 'Default 1 Print Portrait',
+
+  pages: [
+    {
+      pageN: {
+        x: pWidth - 50,
+        y: 30,
+        size: 12,
+      },
+      dim: [pWidth, pHeight],
+
+      images: [createImage(img_x, img_y1, img_width, img_height)],
+    },
+  ],
+};
