4f1b4a1e67b2df74041831d91a7040fc22f4ace7
OwenTruong <44269025+OwenTruong@users.noreply.github.com>
2023-01-09 21:01:32 -0500

    fixed wrapper to make it curry, and now new bug comes out

--- src/Handout/Handout.ts
+++ src/Handout/Handout.ts
@@ -70,25 +70,23 @@ export class Handout {
     const pictures: PDFEmbeddedPicture[] = [];
 
     // TODO: Complete wrapper for jpg, jpeg, png, and pdf
-    const wrapper = async <T>(
-      errorMsg: string,
-      executeable: () => T
-    ): Promise<T | undefined> => {
-      try {
-        return await executeable();
-      } catch (_) {
-        console.error(errorMsg);
-        return undefined;
-      }
+    const wrapper = (errorMsg: string) => {
+      return async <T>(executeable: () => T): Promise<T | undefined> => {
+        try {
+          return await executeable();
+        } catch (_) {
+          // FIXME: Cannot read properties of undefined (reading 'context')
+          console.log(_);
+          console.error(errorMsg);
+          return undefined;
+        }
+      };
     };
 
     // TODO: Object.bind doesn't work well with typescript and trying to map certain asset types(jpg, jpeg, png and pdf) to certain functions doesn't work...
     for (let i = 0; i < assets.length; i++) {
       const asset = assets[i];
-      const verify = wrapper.bind(
-        undefined,
-        `ERROR: Invalid ${asset.type} at asset #${i}`
-      );
+      const verify = wrapper(`ERROR: Invalid ${asset.type} at asset #${i}`);
       if (asset.type === 'jpg' || asset.type === 'jpeg') {
         const picture: PDFImage | undefined = await verify(
           this.#document.embedJpg.bind(undefined, asset.bytes)
@@ -99,9 +97,8 @@ export class Handout {
           type: 'image',
         });
       } else if (asset.type === 'png') {
-        const picture: PDFImage | undefined = await wrapper(
-          this.#document.embedPng.bind(undefined, asset.bytes),
-          `ERROR: Invalid ${asset.type} at asset #${i}`
+        const picture: PDFImage | undefined = await verify(
+          this.#document.embedPng.bind(undefined, asset.bytes)
         );
         if (picture === undefined) continue;
         pictures.push({
@@ -109,9 +106,8 @@ export class Handout {
           type: 'image',
         });
       } else if (asset.type === 'pdf') {
-        const srcPages: PDFPage[] | undefined = await wrapper(
-          this.#getPdfPages.bind(undefined, asset.bytes),
-          `ERROR: Invalid ${asset.type} at asset #${i}`
+        const srcPages: PDFPage[] | undefined = await verify(
+          this.#getPdfPages.bind(undefined, asset.bytes)
         );
         if (srcPages === undefined) continue;
         const embeddedPages: PDFEmbeddedPicture[] = await Promise.all(
